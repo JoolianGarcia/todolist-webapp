e51fb6c142202757823052f97fffb8ffa1177ee3
Julian Garcia <juliangarcia.email@gmail.com>
2018-01-21 17:01:39 +0100

    screen output cleanup

--- script.js
+++ script.js
@@ -3,41 +3,22 @@ var todoList = {
   //intial array of Tasks
   todos: [],
 
-  //Method that displays array of tasks in a readable way
-  displayTodos: function(){
-      if(this.todos.length === 0){
-        console.log("Your To Do List is empty");
-     } else {
-       console.log('Things to do:');
-       for (var i= 0; i < this.todos.length; i++){
-         if(this.todos[i].completed === true){
-           console.log("(x) ", this.todos[i].todoText);
-         } else if(this.todos[i].completed === false){
-           console.log("( ) ", this.todos[i].todoText);
-         }
-        }
-      }
-    },
-
   //Adds to tasks
   addTodo: function(todoText){
     this.todos.push({
       todoText: todoText,
       completed: false,
     });
-    this.displayTodos();
   },
 
   //Changes task's name
   changeTodo: function(position, todoText){
     this.todos[position].todoText = todoText;
-    this.displayTodos();
   },
 
   //Changes completedness of the task
   toggleCompleted: function(position){
     this.todos[position].completed = true;
-     this.displayTodos();
   },
 
   //Toggles completedness of all tasks
@@ -65,13 +46,11 @@ var todoList = {
       this.todos[i].completed = true;
       }
     }
-     this.displayTodos();
   },
 
   //Deletes specific task
   deleteTodo: function(position){
     this.todos.splice(position,1);
-    this.displayTodos();
   },
 
 };
