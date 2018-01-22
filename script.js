07d70d2acd54cbec0b27879c58e3666130e44f41
Julian Garcia <juliangarcia.email@gmail.com>
2018-01-22 22:23:13 +0100

    delete button for each li item

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var todoList = {
   todos: [],
 
   //Adds to tasks
-  addTodo: function(todoText){
+  addTodo: function (todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false,
@@ -12,45 +12,45 @@ var todoList = {
   },
 
   //Changes task's name
-  changeTodo: function(position, todoText){
+  changeTodo: function (position, todoText) {
     this.todos[position].todoText = todoText;
   },
 
   //Changes completedness of the task
-  toggleCompleted: function(position){
+  toggleCompleted: function (position) {
     this.todos[position].completed = true;
   },
 
   //Toggles completedness of all tasks
   //If all complete -> all incompleted
   //Else if some complete or non complete -> all completed
-  toggleAll: function(){
+  toggleAll: function () {
 
     var totalTodos = this.todos.length;
     var completedTodos = 0;
 
     //Make the counter
-    for(var i = 0 ; i < totalTodos;i++){
-      if(this.todos[i].completed === true){
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
         completedTodos++;
       }
     }
 
-    if(completedTodos === totalTodos){
-      for(var i = 0; i < totalTodos; i++){
-      this.todos[i].completed = false;
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
       }
 
     } else {
-      for(var i = 0; i < totalTodos; i++){
-      this.todos[i].completed = true;
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
       }
     }
   },
 
   //Deletes specific task
-  deleteTodo: function(position){
-    this.todos.splice(position,1);
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
   },
 
 };
@@ -58,16 +58,16 @@ var todoList = {
 
 //Object that includes all methods for buttons
 //Separates functions for UI
-var handlers = { 
+var handlers = {
 
   //Triggers todoList.toggleAll(); via a button
-  toggleAll: function(){
+  toggleAll: function () {
     todoList.toggleAll();
     view.displayTodos();
   },
 
-//Triggers add.toggleAll(); via a button, adds Todo to list
-  addTodo: function(){
+  //Triggers add.toggleAll(); via a button, adds Todo to list
+  addTodo: function () {
     //scoops text from text box
     var newToDo = document.getElementById('addTodoTextInput');
     todoList.addTodo(newToDo.value);
@@ -77,26 +77,26 @@ var handlers = {
   },
 
   //Triggers todoList.changeTodo(); via a button. Inserts new text in give numeric position
-  changeTodo: function(){
+  changeTodo: function () {
     var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
     var changeTodoText = document.getElementById('changeTodoText');
-    todoList.changeTodo(changeTodoPositionInput.valueAsNumber,changeTodoText.value);
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoText.value);
     changeTodoPositionInput.value = '';
     changeTodoText.value = '';
     view.displayTodos();
   },
-  
-   //Triggers todoList.toggleAll(); via a button
-   deleteTodo: function(){
+
+  //Triggers todoList.toggleAll(); via a button
+  deleteTodo: function () {
     var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-     deleteTodoPositionInput.value =  '';
+    deleteTodoPositionInput.value = '';
     view.displayTodos();
-   
+
   },
 
   //Triggers todoList.toggleCompleted(); via a button. Takes number value to spcify which todo to toggle complete
-  toggleCompleted: function(){
+  toggleCompleted: function () {
     var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
     toggleCompletedPositionInput = '';
@@ -106,27 +106,33 @@ var handlers = {
 
 //Responsible for screen output functions
 var view = {
-  
-  displayTodos: function(){
+
+  displayTodos: function () {
     var todoUL = document.querySelector('ul');
     todoUL.innerHTML = '';
-    for(var i = 0; i < todoList.todos.length; i++){
+    for (var i = 0; i < todoList.todos.length; i++) {
       var todoLi = document.createElement('li');
+      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
-      if(todoList.todos[i].completed === true){
-        todoTextWithCompletion = '(x) ';
-      } else {todoTextWithCompletion = '( ) '};
+      if (todoList.todos[i].completed === true) {
+        todoTextWithCompletion = '(x) ' + todo.todoText;
+      } else {
+        todoTextWithCompletion = '( ) ' + todo.todoText
+      };
 
 
-      todoLi.textContent = todoTextWithCompletion + todoList.todos[i].todoText + this.createDeleteButton();;
+      todoLi.textContent = todoTextWithCompletion;
+      todoLi.id = i;
+      todoLi.appendChild(this.createDeleteButton());
       todoUL.appendChild(todoLi);
     };
   },
 
-  createDeleteButton: function(){
+  createDeleteButton: function () {
     var deleteButton = document.createElement('button');
-    // deleteButton.value = 'Delete Todo';
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButtons'
     return deleteButton;
 
   }
